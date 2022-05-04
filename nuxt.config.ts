49bf9d58c9f343362ebcbd6cdcea37e64d5456c0
Mr Footwork <pandau.ting@gmail.com>
2022-05-04 23:32:00 +0200

    added nitro preset for deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,16 @@
 import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
-export default defineNuxtConfig({
-	// ssr breaks on windows for "npm run generate"
-	// https://github.com/nuxt/framework/issues/4555
-	// ssr: false,
-})
+// export default defineNuxtConfig({
+// 	// ssr breaks on windows for "npm run generate"
+// 	// https://github.com/nuxt/framework/issues/4555
+// 	// ssr: false,
+// })
+
+// preset for deploying
+// https://v3.nuxtjs.org/guide/deploy/presets/
+export default {
+	nitro: {
+		preset: 'node-server',
+	},
+}

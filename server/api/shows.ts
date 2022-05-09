aeb2bf5c7bc9bef2d4eb721bae1856095b2697e0
Mr Footwork <pandau.ting@gmail.com>
2022-05-09 09:11:17 +0200

    still testing

--- server/api/shows.ts
+++ server/api/shows.ts
@@ -7,10 +7,6 @@ export default async (req: IncomingMessage, res: ServerResponse) => {
 	const queryObject = url.parse(req.url as string, true).query
 	let data = { data: [{ data: '' }] }
 
-	// test heroku env value
-	console.log(uri)
-	// FIXME connect DB to this app
-
 	const { search } = queryObject
 	if (search) {
 		data = await $fetch(`https://api.tvmaze.com/search/shows?q=${search}`)

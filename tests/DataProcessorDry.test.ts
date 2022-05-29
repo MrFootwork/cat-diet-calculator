19b0d9046ad7b6bdfaed6095ba850d1089ac6013
Mr Footwork <pandau.ting@gmail.com>
2022-05-29 10:37:58 +0200

    solved relative path issue in vitest
    https://github.com/vitest-dev/vitest/discussions/1285
    created alias.ts
    created vitest.config.ts

--- tests/DataProcessorDry.test.ts
+++ tests/DataProcessorDry.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'vitest'
-import DataProcessorDry from '@/model/DataProcessorDry'
+import DataProcessorDry from '~~/model/DataProcessorDry'
 
 const DataProcessor = DataProcessorDry.getInstance()
 

f05f2cdf137e9b55f74434b37452d29de97bedba
Mr Footwork <pandau.ting@gmail.com>
2022-06-02 22:32:11 +0200

    loading text

--- pages/index.vue
+++ pages/index.vue
@@ -2,76 +2,78 @@
 <!-- FIXME build components-->
 <template>
   <div>
-    <div class="calculator-ui">
-      <div class="cat-weight">
-        <label for="cat-weight">{{ calculator.catWeight }}</label>
-        <input id="cat-weight" type="range" min="3" max="6" v-model.number="calculator.catWeight" />
-      </div>
 
-      <div class="cat-shape">
-        <label for="ideal">
-          <input type="radio" name="catShape" id="ideal" value="ideal" v-model="calculator.catShape" />
-          Ideal
-        </label>
-        <label for="overweight">
-          <input type="radio" name="catShape" id="overweight" value="overweight" v-model="calculator.catShape" />
-          Overweight
-        </label>
-        <button class="cat-shape-help-toggle" @click="toggleCatShapeHelp">
-          What is my cat?
-        </button>
-      </div>
-
-      <div class="cat-shape-help" v-if="showHelp">
-        <img src="/HealthyCatWeight.webp" alt="Can you see/feel the ribs of your cat?" />
-      </div>
-
-      <div class="dry-food">
-        <div class="dry-food-card" v-for="dryFood in calculator.brandsOfType('dry')" :key="dryFood._id">
-          <label :for="dryFood.name">
-            <img :src="dryFood._id" :alt="dryFood.name" />
-            <input type="checkbox" :id="dryFood.name" v-model="dryFood.isMixPortion" />
-            {{ dryFood.name }}
-          </label>
-        </div>
-      </div>
-
-      <div class="dry-mix">
-        <div class="dry-mix-slider" v-for="(dryFood, i) in calculator
-        .brandsOfType('dry')
-        .filter(brand => brand.isMixPortion)" :key="dryFood._id">
-          <label for="dry-food-brand-name">{{ dryFood.name }}</label>
-          <input id="dry-food-brand-name" type="range" min="0" max="1" step=".1" v-model.number="dryFood.mixPortion" />
-          <label for="dry-food-brand-name">{{ dryFood.mixPortion }}</label>
-        </div>
-      </div>
-
-      <div class="wet-food">
-        <div class="wet-food-card" v-for="wetFood in calculator.brandsOfType('wet')" :key="wetFood._id">
-          <label :for="wetFood.name">
-            <img :src="wetFood._id" :alt="wetFood.name" />
-            <input type="checkbox" :id="wetFood.name" v-model="wetFood.isMixPortion" />
-            {{ wetFood.name }}
-            <input type="number" min="0" step=".5" v-model.number="wetFood.mixPortion" v-if="wetFood.isMixPortion" />
-          </label>
-        </div>
-      </div>
+    <div v-if="isLoading">
+      ...loading
     </div>
-
-    <div>
+    <div v-else>
+      <div class="calculator-ui">
+        <div class="cat-weight">
+          <label for="cat-weight">{{ calculator.catWeight }}</label>
+          <input id="cat-weight" type="range" min="3" max="6" v-model.number="calculator.catWeight" />
+        </div>
+        <div class="cat-shape">
+          <label for="ideal">
+            <input type="radio" name="catShape" id="ideal" value="ideal" v-model="calculator.catShape" />
+            Ideal
+          </label>
+          <label for="overweight">
+            <input type="radio" name="catShape" id="overweight" value="overweight" v-model="calculator.catShape" />
+            Overweight
+          </label>
+          <button class="cat-shape-help-toggle" @click="toggleCatShapeHelp">
+            What is my cat?
+          </button>
+        </div>
+        <div class="cat-shape-help" v-if="showHelp">
+          <img src="/HealthyCatWeight.webp" alt="Can you see/feel the ribs of your cat?" />
+        </div>
+        <div class="dry-food">
+          <div class="dry-food-card" v-for="dryFood in calculator.brandsOfType('dry')" :key="dryFood._id">
+            <label :for="dryFood.name">
+              <img :src="dryFood._id" :alt="dryFood.name" />
+              <input type="checkbox" :id="dryFood.name" v-model="dryFood.isMixPortion" />
+              {{ dryFood.name }}
+            </label>
+          </div>
+        </div>
+        <div class="dry-mix">
+          <div class="dry-mix-slider" v-for="(dryFood, i) in calculator
+          .brandsOfType('dry')
+          .filter(brand => brand.isMixPortion)" :key="dryFood._id">
+            <label for="dry-food-brand-name">{{ dryFood.name }}</label>
+            <input id="dry-food-brand-name" type="range" min="0" max="1" step=".1"
+              v-model.number="dryFood.mixPortion" />
+            <label for="dry-food-brand-name">{{ dryFood.mixPortion }}</label>
+          </div>
+        </div>
+        <div class="wet-food">
+          <div class="wet-food-card" v-for="wetFood in calculator.brandsOfType('wet')" :key="wetFood._id">
+            <label :for="wetFood.name">
+              <img :src="wetFood._id" :alt="wetFood.name" />
+              <input type="checkbox" :id="wetFood.name" v-model="wetFood.isMixPortion" />
+              {{ wetFood.name }}
+              <input type="number" min="0" step=".5" v-model.number="wetFood.mixPortion" v-if="wetFood.isMixPortion" />
+            </label>
+          </div>
+        </div>
+      </div>
       <div>
-        <h5>calculator:</h5>
-        <p>cat weight: {{ calculator.catWeight }}</p>
-        <p>cat shape: {{ calculator.catShape }}</p>
-        <p>Result: {{ calculator.getResult(calculator.allBrands) }}</p>
+        <div>
+          <h5>calculator:</h5>
+          <p>cat weight: {{ calculator.catWeight }}</p>
+          <p>cat shape: {{ calculator.catShape }}</p>
+          <p>Result: {{ calculator.getResult(calculator.allBrands) }}</p>
+        </div>
+        <!-- <div>
+          <h5>calculator data:</h5>
+          <p>{{ JSON.stringify(calculator.allBrands) }}</p>
+        </div> -->
+        <button @click="refreshData">Refresh Data</button>
+        <button @click="reset">Reset Database</button>
       </div>
-      <!-- <div>
-        <h5>calculator data:</h5>
-        <p>{{ JSON.stringify(calculator.allBrands) }}</p>
-      </div> -->
-      <button @click="refreshData">Refresh Data</button>
-      <button @click="reset">Reset Database</button>
     </div>
+
   </div>
 </template>
 
@@ -84,10 +86,11 @@ const db = ref(Database.getInstance())
 const calculator = ref(Calculator.getInstance())
 
 const showHelp = ref(false)
+const isLoading = ref(true)
 
 onBeforeMount(async () => {
   await calculator.value.refresh()
-
+  isLoading.value = false
 })
 
 function toggleCatShapeHelp() {
@@ -95,11 +98,15 @@ function toggleCatShapeHelp() {
 }
 
 async function refreshData() {
+  isLoading.value = true
   await calculator.value.refresh()
+  isLoading.value = false
 }
 
 async function reset() {
+  isLoading.value = true
   await db.value.resetDB()
+  isLoading.value = false
 }
 </script>
 

832996042857d475724068d5acccc09cc4ed598e
Mr Footwork <pandau.ting@gmail.com>
2022-06-17 09:33:49 +0200

    added placeholder images

--- components/DryFoodSelector.vue
+++ components/DryFoodSelector.vue
@@ -3,7 +3,8 @@
     <div class="dry-food">
       <div class="dry-food-card" v-for="dryFood in calculator.brandsOfType('dry')" :key="dryFood._id">
         <label :for="dryFood.name">
-          <img :src="dryFood._id" :alt="dryFood.name" />
+          <!-- <img :src="dryFood._id" :alt="dryFood.name" /> -->
+          <img :src="urlPlaceholder(dryFood)" :alt="dryFood.name" />
           <input type="checkbox" :id="dryFood.name" v-model="dryFood.isMixPortion" />
           {{ dryFood.name }}
         </label>
@@ -13,10 +14,19 @@
 </template>
 
 <script setup lang="ts">
+import FoodBrand from '~~/model/IFoodBrand';
 import Calculator from '~~/model/MCalculator'
 
 const calculator = ref(Calculator.getInstance())
 
+const urlPlaceholder = function (dryFood: FoodBrand) {
+
+  const baseUrl = 'https://via.placeholder.com/100x150'
+  const query = `?text=${dryFood.name}`
+
+  return baseUrl + query
+
+}
 </script>
 
 <style scoped>

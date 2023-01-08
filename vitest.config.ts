f5d63497b00c28c8b810f640d044b6144545137f
Mr Footwork <pandau.ting@gmail.com>
2023-01-08 09:47:31 +0100

    added quokka with vite-node reminder

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import { alias } from './alias'
 
+// TODO add quokka functionality
+// https://quokkajs.com/whatsnew/vite-node-runtime.html
+// https://www.npmjs.com/package/vite-node
+
 export default defineConfig({
 	root: '.',
 	esbuild: {

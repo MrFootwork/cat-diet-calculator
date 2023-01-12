5f748856448a44f5fb3f54d55a57208283ae8cd9
MrFootwork <pandau.ting@gmail.com>
2023-01-12 22:33:57 +0100

    number input styling for wet food cards

--- components/SelectorFoodWet.vue
+++ components/SelectorFoodWet.vue
@@ -1,7 +1,23 @@
 <script setup lang="ts">
+import FoodBrand from '~~/model/IFoodBrand';
 import Calculator from '~~/model/MCalculator';
 
 const calculator = ref(Calculator.getInstance());
+
+const step = .5;
+const minimum = 0;
+
+function amountChange(brand: FoodBrand, operator: "+" | "-") {
+
+  if (operator === "+") (brand.mixPortion as number) += step;
+
+  if (operator === "-" && (brand.mixPortion as number) === 0) return;
+  if (operator === "-") (brand.mixPortion as number) -= step;
+}
+
+function amountReset(brand: FoodBrand) {
+  (brand.mixPortion as number) = 0;
+}
 </script>
 
 <template>
@@ -17,14 +33,20 @@ const calculator = ref(Calculator.getInstance());
                :id="wetFood.name"
                v-model="wetFood.isMixPortion" />
 
-        <div class="input-number">
-          <button class="input-number-minus"></button>
-          <input v-if="wetFood.isMixPortion"
-                 type="number"
-                 min="0"
-                 step=".5"
-                 v-model.number="wetFood.mixPortion" />
-          <button class="input-number-plus"></button>
+        <div class="input-number"
+             v-if="wetFood.isMixPortion">
+          <div class="input-number container">
+            <button class="input-number-minus"
+                    @click="amountChange(wetFood, '-')"
+                    @dblclick="amountReset(wetFood)"
+                    title="Double click to set to 0.">-</button>
+            <input type="number"
+                   :min="minimum"
+                   :step="step"
+                   v-model.number="wetFood.mixPortion" />
+            <button class="input-number-plus"
+                    @click="amountChange(wetFood, '+')">+</button>
+          </div>
         </div>
 
         <label :for="wetFood.name">{{ wetFood.name }}</label>
@@ -35,6 +57,7 @@ const calculator = ref(Calculator.getInstance());
 
 <style scoped lang="scss">
 @use 'mixins' as *;
+@use 'colors' as *;
 
 .wet-food {
   display: flex;
@@ -76,33 +99,93 @@ const calculator = ref(Calculator.getInstance());
 
       input[type=checkbox] {
         visibility: hidden;
+        display: none;
       }
 
       .input-number {
-        & .input-number-minus {}
 
-        input[type=number] {
-          position: absolute;
+        display: flex;
+        justify-content: center;
+        align-items: center;
 
-          bottom: 0;
-          right: 0;
-          z-index: 100;
+        position: absolute;
 
-          // hide spin buttons for
-          // firefox 
-          -moz-appearance: textfield;
-          appearance: textfield;
-          margin: 0;
+        z-index: 10;
+        bottom: 5%;
+        width: 100%;
 
-          // and for chrome
-          &::-webkit-inner-spin-button {
-            -webkit-appearance: none;
+        & .input-number.container {
+
+          display: flex;
+          justify-content: center;
+          align-items: center;
+
+          width: 40%;
+          min-width: 6rem;
+          border-radius: 5rem;
+          padding: .3rem;
+          background-color: $light-primary-color;
+          opacity: 90%;
+
+          font-weight: 600;
+          font-size: 1.2rem;
+
+          input[type=number] {
+
+            text-align: center;
+            width: 2rem;
+            background-color: $light-primary-color;
+            outline: none;
+            border: none;
+            // color: white;
+            font-weight: inherit;
+            font-size: inherit;
+
+            // hide spin buttons for
+            // firefox 
+            -moz-appearance: textfield;
+            appearance: textfield;
+            margin: 0;
+
+            // and for chrome
+            &::-webkit-inner-spin-button {
+              -webkit-appearance: none;
+            }
+          }
+
+          button {
+            width: 1.7rem;
+            height: 1.7rem;
+            border-radius: 50%;
+
+            background: none;
+            border: none;
+            cursor: pointer;
+
+            font-weight: inherit;
+            font-size: inherit;
+            text-align: center;
+
+
+            &:hover {
+              box-shadow: -4px 3px 15px -5px black;
+            }
+
+            &:active {
+              box-shadow: inset -2px 2px 9px -5px black;
+
+            }
+          }
+
+          .input-number-minus {
+            margin-right: .1rem;
+          }
+
+          .input-number-plus {
+            margin-left: .1rem;
           }
         }
-
-        & .input-number-plus {}
       }
-
     }
   }
 }

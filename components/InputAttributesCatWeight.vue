18b94ed982f71aa4ebe5e7129315106d30f5e21d
MrFootwork <pandau.ting@gmail.com>
2023-01-17 23:37:19 +0100

    made label position dynamic on sliderposition

--- components/InputAttributesCatWeight.vue
+++ components/InputAttributesCatWeight.vue
@@ -6,13 +6,36 @@ const weightMinimum = computed(() => calculator.value.weightProperties.minimum);
 const weightMaximum = computed(() => calculator.value.weightProperties.maximum);
 const weightSteps = computed(() => calculator.value.weightProperties.steps);
 
+const sliderValue = computed(() => {
+  return calculator.value.catWeight;
+});
+
+const sliderPosition = computed(() => {
+  const thisPosition = (
+    (calculator.value.catWeight - weightMinimum.value) /
+    (weightMaximum.value - weightMinimum.value)
+  );
+
+  if (sliderValue.value === weightMaximum.value) {
+    return ((thisPosition * 100) + 2) + "px";
+  } else if (sliderValue.value === weightMaximum.value) {
+    return ((thisPosition * 100) - 2) + "px";
+  } else {
+    return ((thisPosition * 100)) + "px";
+  }
+
+  return ((thisPosition * 100) - 2) + "px";
+});
 </script>
 
 <template>
   <!-- FIXME create seperate range slider component -->
   <!-- inspiration: https://codepen.io/kdbkapsere/pen/vRmVZX -->
   <div class="cat-weight input-range">
-    <label for="cat-weight">{{ calculator.catWeight }}</label>
+    <label for="cat-weight"
+           :style="{ left: sliderPosition }">
+      {{ calculator.catWeight }}
+    </label>
     <input id="cat-weight"
            type="range"
            :min="weightMinimum"
@@ -36,13 +59,15 @@ div.cat-weight.input-range {
 
   &>label {
     // border: 3px solid violet;
+    position: absolute;
+    top: 0;
     width: 2rem;
     text-align: center;
   }
 
   &>input[type=range] {
     border: 3px solid red;
-    width: calc(2 * 4rem);
+    width: calc(2 * $size-button + 1px);
   }
 }
 </style>
